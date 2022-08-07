88e24202abe4bb4e5805fa3dad5021a74abf7ad8
hyunju Shin <designgip@naver.com>
2022-08-07 18:10:03 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,3 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>파이어베이스 테스트앱입니다.</h1>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
